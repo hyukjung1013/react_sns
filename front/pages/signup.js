5038412e1d3ce30ee552c54cdc8f33040e35ebe9
hyukjung <hyukjung1013@gmail.com>
2019-07-18 12:34:46 +0900

    26th commit: How to use ‘next Router’.

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,7 +1,8 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { Form, Checkbox, Input, Button } from 'antd' 
 import { useDispatch, useSelector } from 'react-redux';
 import { SIGN_UP_REQUEST } from '../reducers/user';
+import Router from 'next/router'
 
 export const useInput = (initValue = null) => {
     const [value, setter] = useState(initValue);
@@ -21,7 +22,14 @@ const Signup = () => {
     const [term, setTerm] = useState(false);
     const [passwordError, setPasswordError] = useState(false);
     const [termError, setTermError] = useState(false);
-    const { isSigningUp } = useSelector(state => state.user);
+    const { isSigningUp, me } = useSelector(state => state.user);
+
+    useEffect(() => {
+        if (me) {
+            alert('로그인했으니 메인페이지로 이동합니다.');
+            Router.push('/');
+        }
+    }, [me && me.id])   // me는 dummydata
 
     const dispatch = useDispatch();
 
