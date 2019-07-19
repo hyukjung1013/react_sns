6690ff0d50d55948fd47df068972c65439c23e0a
hyukjung <hyukjung1013@gmail.com>
2019-07-19 15:22:24 +0900

    29th commit: Express routers were designed.

--- back/index.js
+++ back/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 
 const db = require('./models');
+const userAPIRouter = require('./routes/user');
+const postAPIRouter = require('./routes/post');
+const postsAPIRouter = require('./routes/posts');
 
 const app = express();
 db.sequelize.sync();
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
+app.use('/api/user', userAPIRouter);
+app.use('/api/post', postAPIRouter);
+app.use('/api/posts', postsAPIRouter);
 
 app.listen(8080, () => {
     console.log('Server is running on localhost:8080');
---
+++ back/routes/posts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const db = require('../models');
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+
+});
+
+router.post('/images', (req, res) => {
+
+});
+
+module.exports = router;
