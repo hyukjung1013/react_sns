e4b94e102dfe4670bd58801dedcdf39911dc3d48
hyukjung <hyukjung1013@gmail.com>
2019-07-24 19:29:05 +0900

    37th commit: How to use ‘Link’.

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -51,14 +51,14 @@ const PostCard = ({ post }) => {
         extra={<Button>팔로우</Button>}
       >
         <Card.Meta
-          avatar={<Link href={`/user/${post.User.id}`}><a><Avatar>{post.User.nickname[0]}</Avatar></a></Link>}
+          avatar={<Link href={{ pathname: '/user', query: { id: post.User.id } }} as={`/user/${post.User.id}`}><a><Avatar>{post.User.nickname[0]}</Avatar></a></Link>}
           title={post.User.nickname}
           description={
             <div>
               {post.content.split(/(#[^\s]+)/g).map((v) => {
                 if (v.match(/#[^\s]+/)) {
                   return (
-                    <Link href={`/hashtag/${v.slice(1)}`} key={v}><a>{v}</a></Link>
+                    <Link href={{ pathname: 'hashtag', query: { tag: v.slice(1) } }} as={`/hashtag/${v.slice(1)}`} key={v}><a>{v}</a></Link>
                   )
                 }
                 return v;
@@ -83,7 +83,7 @@ const PostCard = ({ post }) => {
                 <li>
                   <Comment
                     author={item.User.nickname}
-                    avatar={<Link href={`/user/${post.User.id}`}><a><Avatar>{item.User.nickname[0]}</Avatar></a></Link>}
+                    avatar={<Link href={{ pathname: '/user', query: { id: item.User.id } }} as={`/user/${item.User.id}`}><a><Avatar>{item.User.nickname[0]}</Avatar></a></Link>}
                     content={item.content}
                   />
                 </li>
