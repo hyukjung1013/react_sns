07f0d0e6900f9f99d3347ce4194aa24a867dc923
hyukjung <hyukjung1013@gmail.com>
2019-07-31 12:26:49 +0900

    56th commit: How to set ‘meta in head tag’ by using ‘react-helmet’. (Search Engine Bots use meta tag.)

--- front/pages/post.js
+++ front/pages/post.js
@@ -2,11 +2,27 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import { LOAD_POST_REQUEST } from '../reducers/post';
+import Helmet from 'react-helmet'
 
 const Post = ({ id }) => {
   const { singlePost } = useSelector(state => state.post);
   return (
     <>
+      <Helmet 
+        title={`${singlePost.User.nickname}님의 글`}
+        description={singlePost.content}
+        meta={[{
+          name: 'description', content: singlePost.content,
+        }, {
+          property: 'og:title', content: `${singlePost.User.nickname}님의 게시글`
+        }, {
+          property: 'og:description', content: singlePost.content
+        }, {
+          property: 'og:image', content: singlePost.Images[0] && `http://localhost:8080/${singlePost.Images[0].src}`
+        }, {
+          property: 'og:url', content: `http://localhost:3030/post/${id}`
+        }]}
+      />
       <div>{singlePost.content}</div>
       <div>{singlePost.User.nickname}</div>
       <div>
