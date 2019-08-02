cfd3cfe74470cdb89e63e1c2a701ed4bcc089a3c
hyukjung <hyukjung1013@gmail.com>
2019-08-02 14:24:35 +0900

    72th commit: Optimization. (FollowList.js)

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -11,6 +11,7 @@ import {
   UNFOLLOW_USER_REQUEST,
 } from '../reducers/user';
 import PostCard from '../containers/PostCard';
+import FollowList from '../components/FollowList';
 
 const Profile = () => {
 
@@ -50,37 +51,19 @@ const Profile = () => {
   return (
     <div>
       <NicknameEditForm />
-      <List
-        style={{ marginBottom: '20px' }}
-        grid={{ gutter: 4, xs: 2, md: 3 }}
-        size="small"
-        header={<div>팔로잉 목록</div>}
-        loadMore={hasMoreFollowing && <Button style={{ width: '100%' }} onClick={loadMoreFollowings}>더 보기</Button>}
-        bordered
-        dataSource={followingList}
-        renderItem={item => (
-          <List.Item style={{ marginTop: '20px' }}>
-            <Card actions={[<Icon key="stop" type="stop" onClick={onUnfollow(item.id)} />]}>
-              <Card.Meta description={item.nickname} />
-            </Card>
-          </List.Item>
-        )}
+      <FollowList
+        header="팔로잉 목록"
+        hasMore={hasMoreFollowing}
+        onClickMore={loadMoreFollowings}
+        data={followingList}
+        onClickStop={onUnfollow}
       />
-      <List
-        style={{ marginBottom: '20px' }}
-        grid={{ gutter: 4, xs: 2, md: 3 }}
-        size="small"
-        header={<div>팔로워 목록</div>}
-        loadMore={hasMoreFollower && <Button style={{ width: '100%' }} onClick={loadMoreFollowers}>더 보기</Button>}
-        bordered
-        dataSource={followerList}
-        renderItem={item => (
-          <List.Item style={{ marginTop: '20px' }}>
-            <Card actions={[<Icon key="stop" type="stop" onClick={onRemoveFollower(item.id)} />]}>
-              <Card.Meta description={item.nickname} />
-            </Card>
-          </List.Item>
-        )}
+      <FollowList
+        header="팔로워 목록"
+        hasMore={hasMoreFollower}
+        onClickMore={loadMoreFollowers}
+        data={followerList}
+        onClickStop={onRemoveFollower}
       />
       <div>
         {mainPosts.map(c => (
